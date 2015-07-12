8b82199ca03a70f0fcbf17d517ada51baf851a2d
hasancse91 <hasan_cse91@yahoo.com>
2015-07-12 13:28:50 +0600

    Add search option on Doctor Search.

--- app/src/main/java/theoaktroop/finddoctor/Diagnostic_Center/DiagnosticCenterListActivity.java
+++ app/src/main/java/theoaktroop/finddoctor/Diagnostic_Center/DiagnosticCenterListActivity.java
@@ -16,25 +16,5 @@ public class DiagnosticCenterListActivity extends ActionBarActivity {
         setContentView(R.layout.activity_diagnostic_center_list);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_diagnostic_center_list, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/theoaktroop/finddoctor/Doctor/DoctorListActivity.java
+++ app/src/main/java/theoaktroop/finddoctor/Doctor/DoctorListActivity.java
@@ -1,9 +1,13 @@
 package theoaktroop.finddoctor.Doctor;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import theoaktroop.finddoctor.R;
 
@@ -16,25 +20,27 @@ public class DoctorListActivity extends ActionBarActivity {
         setContentView(R.layout.activity_doctor_list);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_doctor_list, menu);
-        return true;
+
+    public void searchAction(View view){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.doctor_search, null);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Searching filter")
+                .setMessage("Input your required searching field for filtering: ")
+                .setView(dialogView)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_search_category_default)
+                .show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
